da9ae8300e83eb7fff5c26dd34dce79599df6deb
tmr <tom.rowlett@gmail.com>
2017-03-31 14:19:35 -0400

    Add Year-end File maintenance

---
+++ maintenance/annual.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="annual.aspx.cs" Inherits="Maintenance_annual" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    <h2>Annual File Maintenance</h2>
+    <p>
+        Purge all prior year entries from Golfer of the Year Database</p>
+    <p>
+        &nbsp;</p>
+        <table style="width: 100%;">
+            <tr>
+                <td>
+                    <asp:Button ID="btnFiles" runat="server" Text="Files" width ="100" OnClick="btnFiles_Click" />
+                    &nbsp;</td>
+                <td><asp:Label ID="lblFiles" runat="server" Text="Purge all entries in GoYFiles"></asp:Label>
+                    &nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td><asp:Button ID="Button1" runat="server" Text="Summary" width ="100" OnClick="Button1_Click" />
+                    &nbsp;</td>
+                <td><asp:Label ID="lblSummary" runat="server" Text="Purge all entries in GoYSummary" /> &nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td><asp:Button ID="btnDetail" runat="server" Text="Detail" width ="100" OnClick="btnDetail_Click" /> &nbsp;</td>
+                <td><asp:Label ID="lblDetail" runat="server" Text="Purge all entries in GoYDetail" />&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+<p>Last updated March 27, 2017.</p>
+
+</asp:Content>
+
---
+++ maintenance/annual.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Maintenance_annual : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private string purgeAllLoadedFileNames()
+    {
+        MrTimeZone etz = new MrTimeZone();
+        DateTime goyToday = etz.eastTimeNow();
+        string rslt = "";
+        string GoYConn = ConfigurationManager.ConnectionStrings["GoYConnect"].ToString();
+        GoY db = new GoY(GoYConn);
+        int countOfEntries = 0;
+        var fi = from gf in db.GoYFiles
+                  where gf.Date_Loaded < goyToday
+                  orderby gf.Date_Loaded
+                  select gf;
+
+
+        foreach (GoYFiles item in fi)
+        {
+           db.GoYFiles.DeleteOnSubmit(item);
+           countOfEntries++;
+        }
+        db.SubmitChanges();
+        rslt = string.Format("{0} Sign Up Entries deleted from GoYFile that were prior to date {1}.", countOfEntries, goyToday.ToShortDateString());
+        return rslt;
+    }
+
+    private string purgeAllGoySummary()
+    {
+        string rslt = "";
+        string highName = "ZZZZZZZZZZ";
+        string GoYConn = ConfigurationManager.ConnectionStrings["GoYConnect"].ToString();
+        GoY db = new GoY(GoYConn);
+        int countOfEntries = 0;
+        var fs = from gs in db.GoYSummary
+                 where gs.MemberName != highName
+                 select gs;
+        foreach (GoYSummary item in fs)
+        {
+            db.GoYSummary.DeleteOnSubmit(item);
+            countOfEntries++;
+        }
+        db.SubmitChanges();
+        rslt = string.Format("{0} Summary entries deleted from GoYSummary Database", countOfEntries);
+        return rslt;
+
+    }
+
+    private string purgeAllGoyDetail()
+    {
+        string rslt = "";
+//        string highName = "ZZZZZZZZZZ";
+        string GoYConn = ConfigurationManager.ConnectionStrings["GoYConnect"].ToString();
+        GoY db = new GoY(GoYConn);
+        int countOfEntries = 0;
+        var fd = from gd in db.GoYDetail
+                 where gd.ClubID != "000"
+                 select gd;
+        foreach(GoYDetail item in fd)
+        {
+            db.GoYDetail.DeleteOnSubmit(item);
+            countOfEntries++;
+        }
+        db.SubmitChanges();
+        rslt = string.Format("{0} Entried deleted from GoYDetail Database", countOfEntries);
+        return rslt;
+    }
+
+    protected void btnFiles_Click(object sender, EventArgs e)
+    {
+        lblFiles.Text = purgeAllLoadedFileNames();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        lblSummary.Text = purgeAllGoySummary();
+    }
+
+    protected void btnDetail_Click(object sender, EventArgs e)
+    {
+        lblDetail.Text = purgeAllGoyDetail();
+    }
+}
